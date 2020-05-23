980600a788dd4bca170153f6a22e9fc80e3629e1
KachurovskyiD <kachurovskyi.d@gmail.com>
2020-05-23 21:55:50 +0300

    first task

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+personalMovieDB.count = numberOfFilms;
+
+let firstAsk = prompt("Один из последних просмотренных фильмов?", ""),
+  firstAskRate = +prompt("Нас сколько оцените его?", ""),
+  secondAsk = prompt("Один из последних просмотренных фильмов?", ""),
+  secondAskRate = +prompt("Нас сколько оцените его?", "");
+personalMovieDB.movies[firstAsk] = firstAskRate;
+personalMovieDB.movies[secondAsk] = secondAskRate;
+
+console.log(personalMovieDB);
