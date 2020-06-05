3a16229548a20730cdb5690876b4fb0fe7da478b
KachurovskyiD <kachurovskyi.d@gmail.com>
2020-06-05 13:29:45 +0300

    end first project

--- project/js/script.js
+++ project/js/script.js
@@ -1,57 +1,94 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-let promoAdv = document.querySelector('.promo__adv'),
-    advItem = promoAdv.querySelectorAll('img'),
-    genre = document.querySelector('.promo__genre'),
-    promoBg = document.querySelector('.promo__bg'),
-    movieList = document.querySelector('.promo__interactive-list'),
-    promoInteractiveItem = document.querySelectorAll('.promo__interactive-item');
+document.addEventListener('DOMContentLoaded', () => {
+
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+    let promoAdv = document.querySelector('.promo__adv'),
+        advItem = promoAdv.querySelectorAll('img'),
+        genre = document.querySelector('.promo__genre'),
+        promoBg = document.querySelector('.promo__bg'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if (newFilm) {
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+            if (favorite) {
+                console.log('Favorite film added...');
+
+            }
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+            createMovieList(movieDB.movies, movieList);
+        }
+
+        event.target.reset();
+
+    });
+
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+
+    //Analog
+    // for (let i = 0; i < advItem.length; i++) {
+    //     advItem[i].remove();
+    // }
+
+    const makeChanges = () => {
+        genre.textContent = 'ДРАМА';
+
+        promoBg.style.backgroundImage = 'url(img/bg.jpg)';
+    };
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+    function createMovieList(films, parent) {
+        parent.innerHTML = "";
+        sortArr(movieDB.movies);
 
 
-advItem.forEach(item => {
-    item.remove();
-});
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1}. ${film}
+                    <div class="delete"></div>
+                </li>
+             `;
+        });
 
-//Analog
-// for (let i = 0; i < advItem.length; i++) {
-//     advItem[i].remove();
-// }
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                createMovieList(movieDB.movies, movieList);
+            });
+        });
+    }
 
-genre.textContent = 'ДРАМА';
+    deleteAdv(advItem);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
 
-promoBg.style.backgroundImage = 'url(img/bg.jpg)';
-
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-movieList.innerHTML = "";
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-    <li class="promo__interactive-item">${i+1}. ${film}
-        <div class="delete"></div>
-    </li>
-    `;
 });
